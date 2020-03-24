bf843e384ee1554307e1e2fc8a5012d919b6c66c
TRAN VIET HUY <tranviethuy01@gmail.com>
2020-03-24 20:56:36 +0700

    update MQTT

--- VIPER v5/ViewController.swift
+++ VIPER v5/ViewController.swift
@@ -14,15 +14,10 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var btnViperDemo: UIButton!
     @IBOutlet weak var btnRealmDemo: UIButton!
-    
     @IBOutlet weak var btnAlamofireDemo: UIButton!
-    
-    
     @IBOutlet weak var btnObjectMapper: UIButton!
-    
     @IBOutlet weak var btnAutoLayout: UIButton!
-    
-    
+    @IBOutlet weak var btnMQTTClient: UIButton!
     
     
     //MARK FUNCTION
@@ -72,189 +67,57 @@ class ViewController: UIViewController {
         
     }
     
+    @IBAction func btnDoMQTTClient(_ sender: Any) {
+        //
+        pushMQTTClientScene()
+    }
     
     
     
     /* viết các hàm tiến hành hiển thị view */
     func pushViperDemoScene(){
-        
-        
-        //chúng ta bắt đầu khởi tạo từ WireFrame
+        //
         if let vc = DishListWireFrame.createDishListViewController(code: "caigido") {
+        //push
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+      
+        
+    }
+    
+    
+    func pushRealmDemoScene() {
+        
+        //tiến hành hiển thị scene RealmDemo
+        if let vc = RealmDishListWireFrame.createRealmDishListViewController(code: "caigido") {
         
             //ssself.title  = "Back K"
             self.navigationController?.pushViewController(vc, animated: true)
         }
-        
-    /*
-    let storyboard = UIStoryboard(name: "ViperDishListStoryboard", bundle: nil)
-    let vc = storyboard.instantiateViewController(withIdentifier: "ViperDishListStoryboardID")
-    //self.present(controller, animated: true, completion:        nil)
-   
-        
-        
-    let nav = self.navigationController
-    
-    nav?.pushViewController(vc, animated: true)
-        //self.navigationController?.pushViewController(vc, animated: true)
-    */
-        
-        
-    }
-    func pushViperDemoScene_2() {
-        print("Bắt đầu khởi tạo ViperDemoScene")
-        
-        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
-        let resultViewController = storyBoard.instantiateViewController(withIdentifier: "ViperDishListStoryboard") as! DishListViewController
-        //self.navigationController?.pushViewController(resultViewController, animated: true)
-        
-    
-    }
-    func pushViperDemoScene_old(){
-        
-        print("Bắt đầu khởi tạo ViperDemoScene")
-        
-        let storyBoard : UIStoryboard = UIStoryboard(name: "ViperDishListStoryboard", bundle: nil)
-        let vc = DishListViewController()
-        //tại đây vẫn chưa có chuyện gì xảy ra nhé
-        //chúng ta chỉ tạo thôi xem sao nhé
-        
-        //tiến hành pushView
-    self.navigationController?.pushViewController(vc, animated: false)
-        
-        //tại đây chúng ta tiến hành khởi tạo
-        
-       // let navigationController = view.naviganationController
-        
-        //let objVC : SecondViewController? = storyboard?.instantiateViewController(withIdentifier: <#T##String#>)
-        //let objVC : SecondViewController? = SecondViewController()
-        //print (objVC)
-        //add to rootView
-        /*
-        let objectNavi = UINavigationController(rootViewController: objVC!)
-        print(objectNavi)
-        */
-        
-        
-        
-        //present
-        //self.present(objectNavi, animated: true)
-        
-        
-        
-        
-        
-        //tiến hành hiển thị scene ViperDemo
-        /*
-        if let vc = ViewController , let navigationController =  {
-        
-         navigationController?.pushViewController(vc, animated: true)
-        }
-         */
-    
-    }
-    
-    func pushRealmDemoScene() {
-    //tiến hành hiển thị scene RealmDemo
-    print("Bắt đầu khởi tạo RealmDemoScene")
-        
-    if let vc = RealmDishListWireFrame.createRealmDishListViewController(code: "caigido") {
-    
-        //ssself.title  = "Back K"
-    self.navigationController?.pushViewController(vc, animated: true)
-    }
-        
-    /*
-        let storyboard = UIStoryboard(name: "RealmDishListStoryboard", bundle: nil)
-         let vc = storyboard.instantiateViewController(withIdentifier: "RealmDishListStoryboardID")
-         //self.present(controller, animated: true, completion:        nil)
-        self.navigationController?.pushViewController(vc, animated: true)
- 
-    */
-        /*
-         let nav = self.navigationController
-         
-         nav?.pushViewController(vc, animated: true)
-        */
-       /*
-        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        
-        print ("GKDEBUG >> storyBoard: \(storyBoard)")
-        
-        print ("GKDEBUG >> self \(self.navigationController)")
-        let realmView = RealmDishListViewController()
-        
-        print ("GKDEBUG >> realmView \(realmView)")
-        
-        //tiến hành navigate
-        self.navigationController?.pushViewController(realmView, animated: true)
-        
-        */
-        /*
-         
-        
-        let storyBoard: UIStoryboard = UIStoryboard(name: "RealmDishListStoryboard", bundle: nil)
-        
-        print ("GKDEBUG >> storyBoard: \(storyBoard)")
-        
-        let vc = storyBoard.instantiateViewController(withIdentifier: "RealmDishListViewController") as! RealmDishListViewController
-        
-       print ("GKDEBUG >> vc: \(vc)")
-        self.navigationController?.pushViewController(vc, animated: true )
-        /*
-        let newViewController = storyBoard.instantiateViewController(withIdentifier: "RealmDishListStoryboard") as! NewViewController
-       
-    self.navigationController?.pushViewController
- (newViewController, animated: true)
-        
-        */
- 
-
- */
-        
+     
     }
     
     func pushAlamofireDemoScene(){
-    //tiến hành hiển thị scene AlamofireDemo
-    print("Bắt đầu khởi tạo AlamofireDemoScene")
-    
+        //show AlamofireDemo
         let storyboard = UIStoryboard(name: "AlamofireDishListStoryboard", bundle: nil)
          let vc = storyboard.instantiateViewController(withIdentifier: "AlamofireDishListStoryboardID")
          //self.present(controller, animated: true, completion:        nil)
         self.navigationController?.pushViewController(vc, animated: true)
         
-        /*
-        let storyBoard : UIStoryboard = UIStoryboard(name: "AlamofireDishListStoryboard", bundle: nil)
-        let vc = AlamofireDishListViewController()
-        //tiến hành push
-        self.navigationController?.pushViewController(vc, animated: true)
-        */
     }
     
     func pushObjectMapperScene(){
-    //tiến hành hiển thị Scene Object Mapper
-    print("Bắt đầu khởi tạo ObjectMapperDemoScene")
-        
+        //
         let storyboard = UIStoryboard(name: "ObjectMapperDishListStoryboard", bundle: nil)
          let vc = storyboard.instantiateViewController(withIdentifier: "ObjectMapperDishListStoryboardID")
-         //self.present(controller, animated: true, completion:        nil)
         self.navigationController?.pushViewController(vc, animated: true)
-        
-        
-        /*
-        let storyBoard : UIStoryboard = UIStoryboard(name: "ObjectMapperDishListViewController", bundle: nil)
-        let vc = ObjectMapperDishListViewController()
-        
-        //push
-        self.navigationController?.pushViewController(vc, animated: true)
-        */
+       
         
     }
     
     
     func pushAutoLayoutScene(){
-        //tiến hành hiển thị Scene Auto Layout
-    print("Bắt đầu khởi tạo AutoLayoutDemoScene")
+        //
         let storyboard = UIStoryboard(name: "AutoLayoutDishListStoryboard", bundle: nil)
          let vc = storyboard.instantiateViewController(withIdentifier: "AutoLayoutDishListStoryboardID")
          //self.present(controller, animated: true, completion:        nil)
@@ -262,17 +125,18 @@ class ViewController: UIViewController {
         
     }
     
+    func pushMQTTClientScene() {
+    
+    
+    }
+    
+    
     class func initWithStoryBoard() ->ViewController? {
     
         //checkmates
         return nil
     }
     
-    /*
-    class func initWithStoryBoard() -> ViewController?{
-        return ViewService.viewController(storyBoardId(), storyboardName: storyBoardName()) as? ViewController
-    }
-    */
 
 }
 
